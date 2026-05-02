0a13f171a5ec52a18651978015ba41715ab2e90d
bhavesh malviya <bhaveshmalviya@bhaveshs-MacBook-Pro.local>
2026-05-02 13:57:36 +0530

    fix: port binding for render

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('/', (req, res) => {
 
 // 🔹 PORT (IMPORTANT for Render)
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT, '0.0.0.0', () => {
+    console.log("Server running on port " + PORT);
+  });
